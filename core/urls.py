39152b7259d5a59e3186450fbc9a52c2bbb4c005
JeffersonGuedes <jeffersonguedes0211@gmail.com>
2024-08-01 20:27:11 -0300

    :tada: Aula18: Criando de app django - foi abordado criação de app no django - orientado o significado de cada arquivo - orientando sobre o inspecionar do browser - e a aula mostrou sobre os status http response - proxima aula vai falar sobre implementar arquivos na view como response para usuário

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,20 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
-def sobre (request):
-    return HttpResponse('legal')
+
+def home(request):
+    return HttpResponse('home')
+
+def sobre(request):
+    return HttpResponse('sobre')
+
+def contato(request):
+    return HttpResponse('contato')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
     path('sobre/', sobre),
+    path('contato/', contato),
 ]
