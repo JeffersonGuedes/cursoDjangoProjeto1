c52b09f45e41545d40188ad6b36b7362be806efd
JeffersonGuedes <jeffersonguedes0211@gmail.com>
2024-08-01 20:47:36 -0300

    :snake: Feat:Implementando view - foi migrado as funções para pasta especifica do view - foi criado um urls no app pra fazer conexão com urls do projeto - foi configurado app no settings do projeto - explicação sobre usar include e manipular rotas

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-def home(request):
-    return HttpResponse('home')
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-def contato(request):
-    return HttpResponse('contato')
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
+    path('', include('home.urls'))
 ]
