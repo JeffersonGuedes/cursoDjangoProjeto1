589dbe3090a7f39dea83c391a80619744b8ef5e0
JeffersonGuedes <jeffersonguedes0211@gmail.com>
2024-08-30 11:55:44 -0300

    :snake: Feat: Criando nova page category

--- home/views.py
+++ home/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from home.models import Recipe
 
 def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'home/pages/home.html', context = {
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=2,
+        is_published=True
+        ).order_by('-id')
+    
+    category_name = (
+        getattr(recipes.first(), 'category', None),
+        'name',
+        'Not found'
+    )
+
+    return render(request, 'home/pages/category.html', context = {
+        'recipes': recipes,
+        'title': f'{category_name}'
     })
 
 def recipe(request, id):
